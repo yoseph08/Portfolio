627d2f90878e6fce4b5263fffc118cfd08325d67
yoseph08 <yoseph.achamyeleh@sstx.org>
2024-12-12 22:46:25 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-particlesJS("background", {
+particlesJS("welcome-background", {
   particles: {
     number: { value: 150, density: { enable: true, value_area: 800 } },
     color: { value: "#ffffff" },
     shape: {
-      type: "triangle",
+      type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
       
@@ -27,7 +27,7 @@ particlesJS("background", {
     },
     move: {
       enable: true,
-      speed: 4,
+      speed: 1.5,
       direction: "none",
       random: false,
       straight: false,
@@ -53,20 +53,93 @@ particlesJS("background", {
   },
   retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+
+
+
+particlesJS("content-background", {
+  particles: {
+    number: { value: 150, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "triangle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 10,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+
+
+
+
+// this makes sure nav bar doesnt overlap the page heading after navigating to a section from the nav bar
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+    
+        const navbarHeight = document.querySelector(".navigation").offsetHeight;
+        const targetPosition = targetSection.offsetTop;
+
+    
+        window.scrollTo({
+          top: targetPosition - navbarHeight -30, 
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
+
+
